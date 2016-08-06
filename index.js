4c84ea381d2a4b1fcefb260923dd7739e695a29e
Miquel Beltran <mike513@gmail.com>
2016-08-06 10:50:06 +0200

    aoba can transtrale any language to japanese

--- index.js
+++ index.js
@@ -91,15 +91,24 @@ controller.hears('hello', 'direct_message', function (bot, message) {
 
 var translate = require('node-google-translate-skidz');
 
-controller.hears('t (.*)', 'direct_message', function(bot, message) {
+controller.hears('\!t ([a-z]{2}) (.*)', ["ambient", "direct_mention", "mention", "direct_message"], function(bot, message) {
   translate({
-    text: message.match[1],
-    source: 'es',
+    text: message.match[2],
+    source: message.match[1],
     target: 'ja'
   }, function(result) {
-    return bot.reply(message, 'Translate: ' + result);
+    return bot.reply(message, ':jp: ' + result);
   });
 });
+
+controller.hears('open the (.*) doors',['message_received'],function(bot,message) {
+  var doorType = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
+  if (doorType === 'pod bay') {
+    return bot.reply(message, 'I\'m sorry, Dave. I\'m afraid I can\'t do that.');
+  }
+  return bot.reply(message, 'Okay');
+});
+
 
 /**
  * AN example of what could be:
