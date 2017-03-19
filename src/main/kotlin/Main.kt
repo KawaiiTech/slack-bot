32f187b22bb99133b437dd4d3ca8253e5c99c2cb
Miquel Beltran <mike513@gmail.com>
2017-03-19 16:23:09 +0100

    Main kotlin code done and works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,30 @@ external fun require(module: String): dynamic
 
 fun main(args: Array<String>) {
     println("Hello, world!")
-    var config = json("json_file_store" to "./db_slack_bot_ci/")
+    val config = json("json_file_store" to "./db_slack_bot_ci/")
     val customIntegration = require("../lib/custom_integrations")
-    val token = if (process.env.TOKEN) process.env.TOKEN else process.env.SLACK_TOKEN
+    val token = process.env.TOKEN ?: process.env.SLACK_TOKEN
     val controller = customIntegration.configure(token, config, ::onInstallation)
+
+    controller.on("rtm_open", {
+        println("** The RTM api just connected!")
+    })
+    controller.on("rtm_close", {
+        println("** The RTM api just closed")
+    })
+    controller.on("bot_channel_join", { bot, message ->
+        bot.reply(message, "I'm here!")
+    })
+    controller.hears("hello", "direct_message", { bot, message ->
+        bot.reply(message, "Hello!")
+    })
 }
 
 fun onInstallation(bot: dynamic, installer: dynamic) {
-    if (installer) {
+    installer?.let {
         bot.startPrivateConversation(json("user" to installer), {
             err, convo ->
-            if (err) println(err) else {
+            if (err != null) println(err) else {
                 convo.say("I am a bot that has just joined your team")
                 convo.say("You must now /invite me to a channel so that I can be of use!")
             }
